2c37620080d75f4a1e5a39cf6a3f1e82a1f015a0
Aditya Singh Rawat <146981927+Adi-Ida404@users.noreply.github.com>
2025-09-02 16:03:56 +0530

    Update embed store for S3

--- src/embed_store.py
+++ src/embed_store.py
@@ -1,6 +1,14 @@
+import os
+import boto3
+import tempfile
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+
+# AWS client
+s3 = boto3.client("s3")
+
 
 def build_vectorstore(documents, persist_path: str="vectorstore"):
     """
@@ -33,14 +41,41 @@ def build_vectorstore(documents, persist_path: str="vectorstore"):
 
     return vector_store
 
-def load_vectorstore(persist_path: str="vectorstore"):
+def upload_vector_to_s3(local_path: str, bucket: str, key: str):
     """
-    Reloads a previously saved FAISS vector store.
+    Uploads a saved FAISS vectorsstore to s3
 
     Args:
-        persist_path (str, optional): The path to the persisted vector store. Defaults to "vectorstore".
+        local_path (str): The local path to the FAISS vector store.
+        bucket (str): The S3 bucket to upload the vector store to.
+        key (str): The S3 key (path) to upload the vector store to.
     """
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
-    return FAISS.load_local(persist_path, embeddings, allow_dangerous_deserialization = True)
+    for root, _, files in os.walk(local_path):
+       for file in files:
+           s3.upload_file(
+               os.path.join(root,file),
+               bucket,
+               f"{key}/{file}"
+           )
+
+def download_vectorstore_from_s3(bucket: str, key: str, local_path: str="vectorstore"):
+    """
+    Downloads a FAISS vector store from S3.
+
+    Args:
+        bucket (str): The S3 bucket to download the vector store from.
+        key (str): The S3 key (path) to download the vector store from.
+        local_path (str, optional): The local path to save the downloaded vector store. Defaults to "vectorstore".
+    """
+    os.makedirs(local_path, exist_ok=True)
+    objects = s3.list_objects_v2(Bucket=bucket, prefix=key)
+    
+    if "Contents" not in objects:
+        raise FileNotFoundError(f"No objects found in bucket {bucket} with prefix {key}")
+    
+    for obj in objects["Contents"]:
+        filename = obj["Key"].split("/")[-1]
+        s3.download_file(bucket, obj["Key"], os.path.join(local_path, filename))
+        
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return FAISS.load_local(local_path, embeddings, allow_dangerous_deserialization=True)
