45403a8cc0a4dfcdfdcb9cf037fd71a468335fb1
Aditya Singh Rawat <146981927+Adi-Ida404@users.noreply.github.com>
2025-09-02 21:09:55 +0530

    Update App for S3

--- src/app.py
+++ src/app.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 import os
 import shutil
+from dotenv import load_dotenv
 
-from src.ingest import load_single_pdf, load_pdfs_from_folder
+from src.ingest import load_pdfs_from_folder
 from src.embed_store import build_vectorstore, upload_vectorstore_to_s3, download_vectorstore_from_s3
 from src.rag_chain import ask_question
+
+# Load environment variables
+load_dotenv()
 
 app = FastAPI(title="Research RAG")
 
@@ -15,6 +19,10 @@ VECTORSTORE_PATH = "vectorstore"
 
 # Ensure data folder exists
 os.makedirs(DATA_FOLDER, exist_ok=True)
+
+# S3 Config
+S3_BUCKET = os.getenv("S3_BUCKET_NAME")
+S3_KEY = "vectorstore"
 
 class QuestionRequest(BaseModel):
     query: str
@@ -59,15 +67,23 @@ async def upload_file(file: UploadFile):
     docs = load_pdfs_from_folder(DATA_FOLDER)
     build_vectorstore(docs, persist_path=VECTORSTORE_PATH)
     
-    return {"message": f"Uploaded {file.filename} and updated vectorstore."}
+    #Upload vectorstore to S3
+    upload_vectorstore_to_s3(VECTORSTORE_PATH, S3_BUCKET, S3_KEY)
+    
+    return {"message": f"Uploaded {file.filename} and updated vectorstore in S3."}
 
 @app.post("/ask")
 async def ask_question_endpoint(request: QuestionRequest):
     """
-    Ask a question to the RAG pipeline
+    Ask a question to the RAG pipeline (vectorstore pulled from S3)
 
     Args:
         request (QuestionRequest): The question to ask.
     """
+    # Always get the latest vectorstore from S3
+    download_vectorstore_from_s3(S3_BUCKET, S3_KEY, VECTORSTORE_PATH)
+    
+    # Run retrieval + Hugging Face API inference
     answer = ask_question(request.query, VECTORSTORE_PATH)
+    
     return {"query": request.query, "answer": answer}
